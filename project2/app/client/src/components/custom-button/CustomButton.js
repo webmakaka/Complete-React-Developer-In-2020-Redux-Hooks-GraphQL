a6ea23360e1c2252dc6b41530582d9c3992bb8ce
Marley <marley.nodejs@gmail.com>
2019-12-19 12:02:05 +0300

    CustomButton Returned to .scss

--- project2/app/client/src/components/custom-button/CustomButton.js
+++ project2/app/client/src/components/custom-button/CustomButton.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { CustomButtonContainer } from 'components/custom-button/CustomButton.styles.js';
+import 'components/custom-button/CustomButton.scss';
 
-const CustomButton = ({ children, ...props }) => (
-  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
 );
 
 export default CustomButton;
