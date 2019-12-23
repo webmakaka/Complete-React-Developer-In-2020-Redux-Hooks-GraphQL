aa55949257716224d17f35ac6864b9b04062b5f1
Marley <marley.nodejs@gmail.com>
2019-12-23 05:20:15 +0300

    Project 2: Master Project | 16. Master Project Advanced Redux + Firebase | 22. Master Project Stripe Payments Part 2 - Backend | 5. Building A Basic Server | Imports support

--- project2/app/api/server.js
+++ project2/app/api/server.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -9,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
