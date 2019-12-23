387dffbd4028cf1e130bb6548fab7ac5ea80106e
Marley <marley.nodejs@gmail.com>
2019-12-23 11:42:38 +0300

    Project 2: Master Project | 16. Master Project Advanced Redux + Firebase | 23. Master Project Context API | 3. Context Consumer + useContext Hook | example 2

--- project2/app/client/src/pages/collection/CollectionPage.js
+++ project2/app/client/src/pages/collection/CollectionPage.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-import { selectCollection } from 'redux/selectors/shop.selectors';
+import React, { useContext } from 'react';
 
 import CollectionItem from 'components/collection-item/CollectionItem';
-
 import CollectionsContext from 'contexts/collections/collections.context';
 
 import 'pages/collection/CollectionPage.scss';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className="collection-page">
-            <h2 className="title">{title}</h2>
-            <div className="items">
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
-
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
 
 export default CollectionPage;
